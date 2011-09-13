1b1d0a3ff187228aba1c161bca5248e252396e51
hzambran <hzambran@bba20e44-bb5e-40af-8310-95e248542625>
2011-09-13 22:28:24 +0000

    'smry' : bugfix in 'smry.data.frame' (when -at least- 1 column was not numeric)

--- R/smry.R
+++ R/smry.R
@@ -7,7 +7,7 @@
 # Author: Mauricio Zambrano-Bigiarini                              #
 ####################################################################
 # Started: 14-Jun-2008;                                            #
-# Updates: 11-Sep-2009 ; 30-Aug-2011                               #
+# Updates: 11-Sep-2009 ; 30-Aug-2011  ; 14-Sep-2011                #
 ####################################################################
 smry <-function(x, ...) UseMethod("smry")
 
@@ -18,93 +18,111 @@ smry.default <- function(x, na.rm=TRUE, digits = max(3, getOption("digits")-3), 
         x <- zoo::coredata(x)
     } # IF end
 
-
     # Creating the resulting object
-    z <- matrix(NA, ncol=1, nrow=13)
+    z <- as.data.frame(matrix(NA, ncol=1, nrow=13))  
 
-    # If 'x' is not numeric but Date
-    if ( class(x) == "Date" ) {
-      z <- as.data.frame(z)
-    } # IF end
+    n <- length(x)
 
+    nas <- is.na(x)
+    nna <- sum(nas)
 
-    if ( class(x) %in% c("numeric", "integer", "Date") ) {
-
-        n <- length(x)
-
-        na.index <- which(is.na(x))
-        nna      <- length(na.index)
-
-        if (na.rm) {
+    if (na.rm) {
           if (nna > 0) {
-            x  <- x[-na.index] }
-        } # IF end
+            x  <- x[!nas] }
+    } # IF end
+        
+    s    <- summary(x, ..., digits=digits)
 
-        s    <- summary(x, ..., digits=digits)
+    if ( class(x) %in% c("numeric", "integer") ) {
+        
+        # min, q1, median, mean, q3, max
+        z[1:6, 1] <- s
 
-        if ( class(x) %in% c("numeric", "integer") ) {
+        z[7,1] <- IQR(x, na.rm = na.rm)	                         # Interquantile Range IQR = Q(0.75) – Q(0.25)
+        z[8,1] <- sd(x, na.rm = na.rm)	                         # Standard Deviation
+        z[9,1] <- sd(x, na.rm = na.rm) / abs(mean(x, na.rm = na.rm)) # Coefficient of variation ( coef. of variation = sd / |mean| )
 
-            z[1,1] <- s[1] # min
-            z[2,1] <- s[2] # q1
-            z[3,1] <- s[3] # median
-            z[4,1] <- s[4] # mean
-            z[5,1] <- s[5] # q3
-            z[6,1] <- s[6] # max
+        #require(e1071) # for the following 2 functions
+        z[10,1] <- e1071::skewness(x, na.rm = na.rm)  # Skewness (using  e1071 package)
+        z[11,1] <- e1071::kurtosis(x, na.rm = na.rm)  # Kurtosis (using  e1071 package)
 
-            z[7,1] <- IQR(x, na.rm = na.rm)	                         # Interquantile Range IQR = Q(0.75) – Q(0.25)
-            z[8,1] <- sd(x, na.rm = na.rm)	                         # Standard Deviation
-            z[9,1] <- sd(x, na.rm = na.rm) / abs(mean(x, na.rm = na.rm)) # Coefficient of variation ( coef. of variation = sd / |mean| )
-
-            #require(e1071) # for the following 2 functions
-            z[10,1] <- e1071::skewness(x, na.rm = na.rm)  # Skewness (using  e1071 package)
-            z[11,1] <- e1071::kurtosis(x, na.rm = na.rm)  # Kurtosis (using  e1071 package)
-
-            z <- round( z, digits)
-
-        } else { # if 'x' is a Date object
-
-            z[1,1] <- as.character(s[1]) # min
-            z[2,1] <- as.character(s[2]) # q1
-            z[3,1] <- as.character(s[3]) # median
-            z[4,1] <- as.character(s[4]) # mean
-            z[5,1] <- as.character(s[5]) # q3
-            z[6,1] <- as.character(s[6]) # max
-
-            z[7,1]  <- NA # IQR
-            z[8,1]  <- NA # sd
-            z[9,1]  <- NA # cv
-            z[10,1] <- NA # sk
-            z[11,1] <- NA # kur
-
-          } # ELSE end
-
+        z <- round( z, digits)
+            
         z[12,1] <- nna # Amount of NA's
         z[13,1] <- n   # Number of elements
 
-    } # IF end
+
+    } else if ( class(x) == "Date" ) { 
+              z <- smry.Date( x, na.rm=na.rm, digits=digits, ... ) 
+           } else z[1:13, 1] <- NA
 
     row.names(z) <- c("Min.", "1st Qu.", "Median", "Mean", "3rd Qu.",
                       "Max.", "IQR", "sd", "cv", "Skewness", "Kurtosis",
                       "NA's", "n")
 
+    names(z) <- deparse(substitute(x))
     return(z)
 
 } # 'smry.default' end
+
+
+smry.Date <- function(x, na.rm=TRUE, digits = max(3, getOption("digits")-3), ...)  {
+
+        # Creating the resulting object
+        z <- as.data.frame(matrix(NA, ncol=1, nrow=13))
+
+        n <- length(x)
+
+        nas <- is.na(x)
+        nna <- sum(nas)
+
+        if (na.rm) {
+          if (nna > 0) {
+            x  <- x[!nas] }
+        } # IF end
+
+        s <- summary(x, ..., digits=digits)
+
+        # min, q1, median, mean, q3, max            
+        z[1:6, 1] <- as.character(s)
+
+        z[12, 1] <- nna # Amount of NA's
+        z[13, 1] <- n   # Number of elements
+
+ 
+    row.names(z) <- c("Min.", "1st Qu.", "Median", "Mean", "3rd Qu.",
+                      "Max.", "IQR", "sd", "cv", "Skewness", "Kurtosis",
+                      "NA's", "n")
+
+    names(z) <- deparse(substitute(x))
+    return(z)
+
+} # 'smry.Date' end
 
 
 ####################################################################
 # Author: Mauricio Zambrano-Bigiarini                              #
 ####################################################################
 # Started: 14-Jun-2008;                                            #
-# Updates: 11-Sep-2009 ; 30-Aug-2011                               #
+# Updates: 11-Sep-2009 ; 30-Aug-2011  ; 14-Sep-2011                #
 ####################################################################
 smry.data.frame <- function(x, na.rm=TRUE, digits = max(3, getOption("digits")-3), ...)  {
 
-    z <- apply(x, MARGIN=2, FUN=smry.default, na.rm=na.rm, digits=digits, ...)
-    
+    # Creating a copy of the original observed values
+    z <- as.data.frame( matrix(NA, nrow=13, ncol=ncol(x)) )
+
+    z[,1:ncol(z)] <- sapply(1:ncol(x), function(j,y) {
+
+       z[,j] <- smry.default(x= y[,j], na.rm=na.rm, digits=digits, ...)
+
+    }, y = x) # sapply END
+
+
     rownames(z) <- c("Min.", "1st Qu.", "Median", "Mean", "3rd Qu.",
                      "Max.", "IQR", "sd", "cv", "Skewness", "Kurtosis",
                      "NA's", "n")
+    colnames(z) <- colnames(x)
+
     return(z)
 
 } # 'smry.data.frame' end
