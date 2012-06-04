f8cb092c60f7e6779b4c206d7dbc8a662c3b5b0a
hzambran <hzambran@bba20e44-bb5e-40af-8310-95e248542625>
2012-06-04 13:16:26 +0000

    'sfreq': now gives the correct sampling frequency for zoo objects that are matrix/data.frame

--- R/sfreq.R
+++ R/sfreq.R
@@ -1,3 +1,9 @@
+# File sfreq.R
+# Part of the hydroTSM R package, http://www.rforge.net/hydroTSM/ ; 
+#                                 http://cran.r-project.org/web/packages/hydroTSM/
+# Copyright 2009-2012 Mauricio Zambrano-Bigiarini
+# Distributed under GPL 2 or later
+
 ################################################################################
 # sfreq: Sampling frequency of a ts/zoo object                                 #
 ################################################################################
@@ -11,14 +17,14 @@
 sfreq <- function(x, min.year=1800) {
 
   # Checking that 'class(x)'
-  valid.class <- c("xts", "zoo", "ts")    
+  valid.class <- c("xts", "zoo")    
   if (length(which(!is.na(match(class(x), valid.class )))) <= 0) 
-     stop("Invalid argument: 'x' must be in c('xts', 'zoo', 'ts')" )
+     stop("Invalid argument: 'x' must be in c('xts', 'zoo')" )
 
   if ( length(x) < 2) stop("Invalid argument: 'length(x)' must be larger than 2 for finding its sampling frequency" )
 
-  t1 <- time(x[1])
-  t2 <- time(x[2])
+  t1 <- zoo::as.Date(time(x)[1])
+  t2 <- zoo::as.Date(time(x)[2])
 
   if ( (( class(t1) == "character" ) & ( as.numeric(t1) > min.year )) | ( class(t1) == "factor" ) ) {
     sfreq <- "annual"
