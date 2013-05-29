6238773964b7681c94530c90533690f4c0fe9764
hzambran <hzambran@bba20e44-bb5e-40af-8310-95e248542625>
2013-05-29 14:47:55 +0000

    'mip': more informative error messages

--- R/mip.R
+++ R/mip.R
@@ -4,11 +4,17 @@
 # Copyright 2010-2013 Mauricio Zambrano-Bigiarini
 # Distributed under GPL 2 or later
 
-####################################################################
-# 'mip': Given any starting and ending dates, it generates:        #
-#        1) a vector with all the months between the two dates, OR #
-#	 2) the amount of months between the two dates                   #
-####################################################################
+################################################################################
+#                                 'mip'                                        #
+################################################################################
+# Given any starting and ending dates, it generates:                           #
+#        1) a vector with all the months between the two dates, OR             #
+#	 2) the amount of months between the two dates                         #
+################################################################################
+# Author : Mauricio Zambrano-Bigiarini                                         #
+# Started: 2008                                                                #
+# Updates: 29-May-2013                                                         #            
+################################################################################
 
 # 'from'    : Starting date for computing the number of dyas. MUST have the date format specified by 'date.fmt'
 # 'to'	    : Ending date for computing the number of dyas. MUST have the date format specified by 'date.fmt'
@@ -21,6 +27,31 @@ mip <- function(from, to, date.fmt="%Y-%m-%d", out.type="seq") {
      # Checking 'out.type'
      if (is.na(match(out.type, c("seq", "nmbr"))))
         stop("Invalid argument: 'out.type' must be of class 'seq' or 'nmbr'")
+        
+     # Converting 'from' into a Date object (if necessary)
+     if (class(from) != "Date") {
+       from.bak <- from
+       from     <- as.Date(from, format=date.fmt)
+     } # IF end
+     
+     # Checking that 'from' is a valid Date object
+     if (is.na(from)) 
+       stop("Invalid argument: 'from' (", from.bak, 
+            ") is not compatible with 'date.ftm' (", date.fmt, ") !")
+     
+     # Converting 'to' into a Date object (if necessary)
+     if (class(to) != "Date") {
+       to.bak <- to
+       to     <- as.Date(to, format=date.fmt)
+     } # IF end
+     
+     # Checking that 'to' is a valid Date object
+     if (is.na(to)) 
+       stop("Invalid argument: 'to' (", to.bak, 
+            ") is not compatible with 'date.ftm' (", date.fmt, ") !")
+     
+     # Checking that 'from' is lower or equal to 'to'
+     if (to < from) stop("Invalid argument: 'from > to' (", from, " > ", to, ")")
 
      # Generating an Annual-regular time series of Dates.
      vec.months <- seq( from=as.Date(from, format=date.fmt), to=as.Date(to, format=date.fmt), by="months" )
