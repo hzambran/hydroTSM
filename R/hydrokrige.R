632db785757b34f167cd9062bca81a9070cf7532
Mauricio Zambrano-Bigiarini <mzb.devel@gmail.com>
2015-07-17 15:23:37 -0300

    hydrokrige: trying to replace the deprecated 'overlay' by 'over'

--- R/hydrokrige.R
+++ R/hydrokrige.R
@@ -452,14 +452,14 @@ hydrokrige.default <- function(x.ts, x.gis,
 
        # Grid-points overlay.
        # Assigning to all the points in 'x.work', the corresponding fields in 'predictors'
-       x.work.ov = overlay(predictors, x.work)
+       x.work.ov = over(x.work, predictors)
 
        # Getting the names of the predictor variables in 'predictors'
-       pnames <- names(x.work.ov@data)
+       pnames <- names(predictors@data)
 
        # copy the predictors values to 'x.work'
        for (i in 1:length(pnames) )
-          x.work[[ pnames[i]] ] <- x.work.ov@data[pnames[i]]
+          x.work[[ pnames[i]] ] <- x.work.ov[[pnames[i]]]
 
        # Removing the NA's in the predictors, because they give rise to erros in the 'krige' function
        # This should not be made in the previous loop becuase it can
@@ -630,8 +630,8 @@ hydrokrige.default <- function(x.ts, x.gis,
         #if (!is.null(IDvar)) x.idw.block@data[[IDvar]] <- NULL
         
         if (  class(predictors) == "SpatialGridDataFrame" ) {
-           tmp.block <- overlay( as(x.idw["var1.pred"], "SpatialPixelsDataFrame"), SubCatchments.shp, fn = mean)
-        } else tmp.block <-  overlay(x.idw["var1.pred"], SubCatchments.shp, fn = mean)
+           tmp.block <- over(SubCatchments.shp, as(x.idw["var1.pred"], "SpatialPixelsDataFrame"), fn = mean)
+        } else tmp.block <-  over(SubCatchments.shp, x.idw["var1.pred"], fn = mean)
 
         #if ( length(x.idw.block@data[["var1.pred"]]) == length( tmp.block[,1]) ) {
         #  x.idw.block@data[["var1.pred"]] <- tmp.block[,1]
@@ -663,8 +663,8 @@ hydrokrige.default <- function(x.ts, x.gis,
         #tmp.block <- overlay( as(x.idw["var1.pred"], "SpatialPixelsDataFrame"), SubCatchments.shp, fn = mean)
 
         if (  class(predictors) == "SpatialGridDataFrame" ) {
-           tmp.block <- overlay( as(x.idw["var1.pred"], "SpatialPixelsDataFrame"), SubCatchments.shp, fn = mean)
-        } else tmp.block <-  overlay(x.idw["var1.pred"], SubCatchments.shp, fn = mean)
+           tmp.block <- over( SubCatchments.shp, as(x.idw["var1.pred"], "SpatialPixelsDataFrame"), fn = mean)
+        } else tmp.block <-  over( SubCatchments.shp, x.idw["var1.pred"], fn = mean)
         
         #if (length(x.idw.block@data[["var1.pred"]]) == length( tmp.block[,1]) ) {
         #  x.idw.block@data[["var1.pred"]] <- tmp.block[,1]
