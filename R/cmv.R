ab7ecbd41fb18fc93d84981937334633454a8156
Mauricio Zambrano-Bigiarini <mzb.devel@gmail.com>
2025-05-03 16:45:35 +0200

    formatting only

--- R/cmv.R
+++ R/cmv.R
@@ -10,67 +10,69 @@
 # This function counts the percentage/amount of missing data in a zoo object, 
 # using a user-defined temporal scale
 
-# 'x'       : zoo object to be analised
-# 'tscale'  : character with the temporal scale to be used for analysing the mssing data.
-#             Valid values for 'tscale' are: 
-#             -) "hourly"   : the percentage/amount of missing values will be given for each 
-#                             hour and ,therefore, the expected time frequency of 'x' must 
-#                             be sub-hourly.
-#             -) "daily"    : the percentage/amount of missing values will be given for each 
-#                             day and, therefore, the expected time frequency of 'x' must 
-#                             be sub-daily (i.e., hourly or sub-hourly).
-#             -) "weekly"   : the percentage/amount of missing values will be given for each 
-#                             week (starting on Monday) and, therefore, the expected time 
-#                             frequency of 'x' must be sub-weekly (i.e., daily, (sub)hourly).
-#             -) "monthly"  : the percentage/amount of missing values will be given for each 
-#                             month and, therefore, the expected time frequency of 'x' must 
-#                             be sub-monthly (i.e., daily, hourly or sub-hourly).
-#             -) "quarterly": the percentage/amount of missing values will be given for each 
-#                             quarter and, therefore, the expected time frequency of 'x' must 
-#                             be sub-quarterly (i.e., monthly, daily, hourly or sub-hourly).
-#             -) "seasonal" : the percentage/amount of missing values will be given for each 
-#                             weather season (see ?time2season) and, therefore, the expected 
-#                             time frequency of 'x' must be sub-seasonal (i.e., monthly, daily, 
-#                             hourly or sub-hourly).
-#             -) "annual"   : the percentage/amount of missing values will be given for each 
-#                             year and, therefore, the expected time frequency of 'x' must 
-#                             be sub-annual (i.e., seasonal, monthly, daily, hourly or sub-hourly).
-#             -) 'out.type' : character indicating how should be returned the missing values 
-#                             for each temporal scale. Valid values for 'out'type' are: 
-#             -) "percentage": the missing values are returned as an real value, representing
-#                              the percentage of missing values in each temporal scale. 
-#             -) "amount"    : the missing values are returned as an integer value, representing
-#                              the absolute amount of missing values in each temporal scale. 
-#             -) 'dec'       : integer indicating the amount of decimal places included in the output. 
-#                              It is only used when "out.type=='percentage'"
-#             -) 'start'     : character, indicating the starting time used for aggregating sub-daily time 
-#                              series into daily ones. 
-#                              It MUST be provided in the format specified by \code{start.fmt}. \cr
-#                              This value is used to define the time when a new day begins (e.g., for some 
-#                              rain gauge stations). \cr
-#                              -) All the values of \code{x} with a time attribute before \code{start} are
-#                                 considered as belonging to the day before the one indicated in the time 
-#                                 attribute of those values. \cr
-#                              -) All the values of \code{x} with a time attribute equal to \code{start} 
-#                                 are considered to be equal 
-#                                 to \code{"00:00:00"} in the output zoo object. \cr
-#                              -) All the values of \code{x} with a time attribute after \code{start} are 
-#                                 considered as belonging to the same day as the one indicated in the time 
-#                                 attribute of those values. \cr
-#                                 It is useful when the daily values start at a time different from
-#                                 \code{"00:00:00"}. Use with caution. See examples.
-#             -) 'start.fmt' : character indicating the format in which the time is provided in \code{start}.
-#                              By default \code{date.fmt=\%H:\%M:\%S}. See \code{format} in 
-#                              \code{\link[base]{as.POSIXct}}.
-#             -) 'tz'        : character, with the specification of the time zone used in both 
-#                              \code{x} and \code{start}. 
-#                              System-specific (see time zones), but \code{""} is the current time zone,
-#                              and \code{"GMT"} is UTC (Universal Time, Coordinated). 
-#                              See \code{\link[base]{Sys.timezone}} and \code{\link[base]{as.POSIXct}}. \cr
-#                              If \code{tz} is missing (the default), it is automatically set to the
-#                              time zone used in \code{time(x)}. \cr
-#                              This argument can be used to force using the local time zone or any other 
-#                              time zone instead of UTC as time zone.
+# 'x'        :  zoo object to be analised
+# 'tscale'   :  character with the temporal scale to be used for analysing the mssing data.
+#               Valid values for 'tscale' are: 
+#             -) "hourly"    :  the percentage/amount of missing values will be given for each 
+#                               hour and ,therefore, the expected time frequency of 'x' must 
+#                               be sub-hourly.
+#             -) "daily"     :  the percentage/amount of missing values will be given for each 
+#                               day and, therefore, the expected time frequency of 'x' must 
+#                               be sub-daily (i.e., hourly or sub-hourly).
+#             -) "weekly"    :  the percentage/amount of missing values will be given for each 
+#                               week (starting on Monday) and, therefore, the expected time 
+#                               frequency of 'x' must be sub-weekly (i.e., daily, (sub)hourly).
+#             -) "monthly"   :  the percentage/amount of missing values will be given for each 
+#                               month and, therefore, the expected time frequency of 'x' must 
+#                               be sub-monthly (i.e., daily, hourly or sub-hourly).
+#             -) "quarterly" :  the percentage/amount of missing values will be given for each 
+#                               quarter and, therefore, the expected time frequency of 'x' must 
+#                               be sub-quarterly (i.e., monthly, daily, hourly or sub-hourly).
+#             -) "seasonal"  :  the percentage/amount of missing values will be given for each 
+#                               weather season (see ?time2season) and, therefore, the expected 
+#                               time frequency of 'x' must be sub-seasonal (i.e., monthly, daily, 
+#                               hourly or sub-hourly).
+#             -) "annual"    :  the percentage/amount of missing values will be given for each 
+#                               year and, therefore, the expected time frequency of 'x' must 
+#                               be sub-annual (i.e., seasonal, monthly, daily, hourly or sub-hourly).
+#             -) 'out.type'  :  character indicating how should be returned the missing values 
+#                               for each temporal scale. Valid values for 'out'type' are: 
+#             -) "percentage":  the missing values are returned as an real value, representing
+#                               the percentage of missing values in each temporal scale. 
+#             -) "amount"    :  the missing values are returned as an integer value, representing
+#                               the absolute amount of missing values in each temporal scale. 
+#             -) 'dec'       :  integer indicating the amount of decimal places included in the output. 
+#                               It is only used when "out.type=='percentage'"
+#             -) 'start'     :  character, indicating the starting time used for aggregating sub-daily time 
+#                               series into daily ones. 
+#                               It MUST be provided in the format specified by \code{start.fmt}. \cr
+#                               This value is used to define the time when a new day begins (e.g., for some 
+#                               rain gauge stations). \cr
+#                               -) All the values of \code{x} with a time attribute before \code{start} are
+#                                  considered as belonging to the day before the one indicated in the time 
+#                                  attribute of those values. \cr
+#                               -) All the values of \code{x} with a time attribute equal to \code{start} 
+#                                  are considered to be equal 
+#                                  to \code{"00:00:00"} in the output zoo object. \cr
+#                               -) All the values of \code{x} with a time attribute after \code{start} are 
+#                                  considered as belonging to the same day as the one indicated in the time 
+#                                  attribute of those values. \cr
+#                                  It is useful when the daily values start at a time different from
+#                                  \code{"00:00:00"}. Use with caution. See examples.
+#             -) 'start.fmt' :  character indicating the format in which the time is provided in \code{start}.
+#                               By default \code{date.fmt=\%H:\%M:\%S}. See \code{format} in 
+#                               \code{\link[base]{as.POSIXct}}.
+#             -) 'tz'        :  character, with the specification of the time zone used in both 
+#                               \code{x} and \code{start}. 
+#                               System-specific (see time zones), but \code{""} is the current time zone,
+#                               and \code{"GMT"} is UTC (Universal Time, Coordinated). 
+#                               See \code{\link[base]{Sys.timezone}} and \code{\link[base]{as.POSIXct}}. \cr
+#                               If \code{tz} is missing (the default), it is automatically set to the
+#                               time zone used in \code{time(x)}. \cr
+#                               This argument can be used to force using the local time zone or any other 
+#                               time zone instead of UTC as time zone.
+#             -) 'start.month': numeric in [1,..,12], representing the starting month (1:Jan, ..., 12:Dec) 
+#                               to be used in the computation of annual values. By default 'start.month=1'.
 
 
 cmv <-function(x, ...) UseMethod("cmv")
